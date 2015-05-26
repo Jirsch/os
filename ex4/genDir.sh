305749ba00ee48118daa336078049c33b4939c8c
jirsch <jirsch@yahoo-inc.com>
2015-05-26 19:39:18 +0300

    add script

---
+++ ex4/genDir.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+rm -f -r /tmp/root/
+rm -f -r /tmp/mount/
+
+
+mkdir /tmp/mount
+mkdir /tmp/root
+mkdir /tmp/root/inDir
+mkdir /tmp/root/secondDir
+
+for i in {1..999999}; do
+    echo $i % 10 >> /tmp/root/inDir/large
+    echo "|" >> /tmp/root/inDir/large
+    if [ $i % 1000 == 0 ]
+    then
+        echo $i >> /tmp/root/inDir/large
+        echo "|" >> /tmp/root/inDir/large
+    fi
+done
+
+echo "1111111111222222222233333333334444444444" > /tmp/root/text
