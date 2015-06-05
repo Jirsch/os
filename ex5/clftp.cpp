c7ba2115ae177344d1bbdfe7fabae538212849b7
orenbm21 <orenbm21@gmail.com>
2015-06-05 15:36:27 +0300

    added ex5 files

---
+++ ex5/clftp.cpp
@@ -0,0 +1,5 @@
+//
+// Created by orenbm21 on 6/5/2015.
+//
+
+#include "clftp.h"
---
+++ ex5/clftp.h
@@ -0,0 +1,15 @@
+//
+// Created by orenbm21 on 6/5/2015.
+//
+
+#ifndef EX5_CLFTP_H
+#define EX5_CLFTP_H
+
+
+class clftp
+{
+
+};
+
+
+#endif //EX5_CLFTP_H
