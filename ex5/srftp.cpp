cb74acdc4a9a9088cccae197a42e7db0d0a8f1aa
orenbm21 <orenbm21@gmail.com>
2015-06-12 13:01:25 +0300

    added client

--- ex5/srftp.cpp
+++ ex5/srftp.cpp
@@ -3,4 +3,27 @@
 //
 
 #include "srftp.h"
+#include "clftp.h"
 
+#define VALID_NUM_OF_ARGS 3
+#define SUCCESS 0
+#define FAILURE 1
+
+bool isValidPort(int port)
+{
+    return port >= 1 && port <= 65535;
+}
+
+bool isValidMaxFile(size_t maxFileSize)
+{
+    return maxFileSize >= 0;
+}
+
+int main(int argc, char* argv[])
+{
+    int validArgs = SUCCESS;
+    if (argc != VALID_NUM_OF_ARGS || !isValidPort(argv[1]) || !isValidMaxFile(argv[2]))
+    {
+        validArgs = FAILURE;
+    }
+}
