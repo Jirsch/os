6eeb659cd62d1d2f89e55edd912fa302c4ed9f2c
jirsch <jirsch@gmail.com>
2015-06-16 17:15:22 +0300

    test file generation and results

---
+++ ex5/genDir.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#rm -f txt
+
+
+for i in {1..1000}; do
+
+    echo -n "bbliblabli" >> txt
+    #echo -n $((i % 10)) >> /tmp/root/inDir/large
+    #echo -n "|" >> /tmp/root/inDir/large
+
+done
+
+#echo -n "1111111111222222222233333333334444444444" > /tmp/root/text
